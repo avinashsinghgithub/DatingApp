7d70121524e0bc12ef25b8dc84b13082988c36c9
Avinash Singh <ASingh103@slb.com>
2020-03-21 15:22:47 +0530

    End of section 3

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,10 +1,14 @@
-﻿using DatingApp.API.Data;
+﻿using System.Text;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+
 namespace DatingApp.API
 {
     public class Startup
@@ -21,6 +25,17 @@ namespace DatingApp.API
         services.AddDbContext<DataContext>(x =>x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         services.AddControllers();
         services.AddCors();
+        services.AddScoped<IAuthRepository,AuthRepository>();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>{
+            options.TokenValidationParameters = new TokenValidationParameters{
+                ValidateIssuerSigningKey =true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.
+                GetBytes(Configuration.GetSection("Appsettings:Token").Value)),
+                ValidateIssuer = false,
+                ValidateAudience= false
+            };
+        });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -40,6 +55,7 @@ namespace DatingApp.API
 
         app.UseRouting();
         app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+        app.UseAuthentication();
         app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
